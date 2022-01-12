0e21b5f801dc3aa812771091f4c7487efb6eacfd
Anderson Pozo <anderson.pozo@upec.edu.ec>
2022-01-12 13:13:30 -0500

    Add information fields in grades pdf report

--- core/reports/views/grades_report/views.py
+++ core/reports/views/grades_report/views.py
@@ -10,7 +10,7 @@ from django.views.generic import FormView
 from weasyprint import CSS, HTML
 
 from config import settings
-from core.reports.forms import ReportForm, Student, NoteDetails, Punctuations, Matter, Period
+from core.reports.forms import ReportForm, Student, NoteDetails, Punctuations, Matter, Period, Cursos
 from core.school.models import Company
 from core.security.mixins import ModuleMixin
 
@@ -48,24 +48,28 @@ class GradesReportView(ModuleMixin, FormView):
         matter = self.request.POST.get('matter', None)
         start_date = self.request.POST['start_date']
         end_date = self.request.POST['end_date']
+
         mat = Matter.objects.get(id=int(matter))
 
-        students = Student.objects.filter(
-            matriculation__period_id=int(period),
-            matriculation__level_id=int(level),
-            matriculation__level__matter=mat
-        )
-        for s in students:
-            punt = Punctuations.objects.filter(
-                student_id=s.id,
-                notedetails__perioddetail__matter_id=int(matter),
-                notedetails__start_date__range=[start_date, end_date]
-            ).order_by('notedetails__start_date')
-            obj = {0: s.user.get_full_name()}
-            for i, p in enumerate(punt):
-                obj[i + 1] = str(p.note)
-            data.append(obj)
-        return data
+        if period and level and matter:
+            students = Student.objects.filter(
+                matriculation__period_id=int(period),
+                matriculation__level_id=int(level),
+                matriculation__level__matter=mat
+            )
+            for s in students:
+                punt = Punctuations.objects.filter(
+                    student_id=s.id,
+                    notedetails__perioddetail__matter_id=int(matter),
+                    notedetails__start_date__range=[start_date, end_date]
+                ).order_by('notedetails__start_date')
+                obj = {0: s.user.get_full_name()}
+                for i, p in enumerate(punt):
+                    obj[i + 1] = str(p.note)
+                data.append(obj)
+            period = Period.objects.get(id=period)
+            level = Cursos.objects.get(id=level)
+        return data, period, level, mat
 
     def post(self, request, *args, **kwargs):
         action = request.POST.get('action', None)
@@ -76,16 +80,23 @@ class GradesReportView(ModuleMixin, FormView):
                 for i in self.get_activities():
                     data.append(i.toJSON())
             elif action == 'search_students':
-                data = self.search_students()
+                students, _, _, _ = self.search_students()
+                data = students
             elif action == 'search_materia':
                 level = request.POST.get('level', None)
                 data = []
                 for m in Matter.objects.filter(level_id=int(level)).order_by('name')[0:10]:
                     data.append(m.toJSON())
             elif action == 'generate_pdf':
+                students, period, lvl, matter = self.search_students()
                 context = {
                     'activities': self.get_activities(),
-                    'students': self.search_students(),
+                    'students': students,
+                    'period': period,
+                    'level': lvl,
+                    'matter': matter,
+                    'start_date': self.request.POST.get('start_date', None),
+                    'end_date': self.request.POST.get('end_date', None),
                     'company': Company.objects.first(),
                     'date_joined': datetime.now().date(),
                     'title': 'Reporte de Notas'
