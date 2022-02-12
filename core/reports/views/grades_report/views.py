894f92efbf8c899239b5ac166da38b94580f5e43
Anderson Pozo <anderson.pozo@upec.edu.ec>
2022-02-12 12:14:49 -0500

    Change grades report

--- core/reports/views/grades_report/views.py
+++ core/reports/views/grades_report/views.py
@@ -1,5 +1,4 @@
 import json
-import os
 from datetime import datetime
 
 from django.http import HttpResponse
@@ -7,12 +6,12 @@ from django.template.loader import get_template
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import FormView
-from weasyprint import CSS, HTML
+from xhtml2pdf import pisa
 
-from config import settings
-from core.reports.forms import ReportForm, Student, NoteDetails, Punctuations, Matter, Period, Cursos
 from core.school.models import Company
 from core.security.mixins import ModuleMixin
+from core.reports.forms import ReportForm, Student, NoteDetails, Punctuations, Matter, Period, Cursos
+from core.reports.utils import link_callback_report
 
 
 class GradesReportView(ModuleMixin, FormView):
@@ -103,10 +102,13 @@ class GradesReportView(ModuleMixin, FormView):
                 }
                 template = get_template('grades_report/pdf.html')
                 html_template = template.render(context).encode(encoding="UTF-8")
-                url_css = os.path.join(settings.BASE_DIR, 'static/lib/bootstrap-4.3.1/css/bootstrap.min.css')
-                pdf_file = HTML(string=html_template, base_url=request.build_absolute_uri()).write_pdf(
-                    stylesheets=[CSS(url_css)], presentational_hints=True)
-                return HttpResponse(pdf_file, content_type='application/pdf')
+                response = HttpResponse(content_type='application/pdf')
+                pisa_status = pisa.CreatePDF(
+                    html_template,
+                    dest=response,
+                    link_callback=link_callback_report
+                )
+                return response
             else:
                 data['error'] = 'No ha ingresado una opción'
         except Exception as e:
