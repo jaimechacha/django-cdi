94881ce9179a52f714f61094f2470c3bb88d2af5
Anderson Pozo <anderson.pozo@upec.edu.ec>
2022-03-13 21:44:53 -0500

    Add input to return amount material

--- core/inventory/static/output/js/list.js
+++ core/inventory/static/output/js/list.js
@@ -20,16 +20,34 @@ $(function () {
                 {data: "material"},
                 {data: "description"},
                 {data: "amount"},
+                {data: "amount"},
             ],
             columnDefs: [
                 {
-                    targets: [-1],
+                    targets: [-2],
                     class: 'text-center',
                     render: function (data, type, row) {
                         return '<b>'+data+'</b>';
                     }
+                },
+                {
+                    targets: [-1],
+                    class: 'text-center',
+                    render: (data, type, row) => {
+                        return '<input type="text" class="form-control input-sm" autocomplete="off" name="cantidad" value="' + row.amount + '">';
+                    }
                 }
-            ]
+            ],
+            rowCallback: function (row, data, index) {
+                const frm = $(row).closest('tr');
+
+                frm.find('input[name="cantidad"]').TouchSpin({
+                    min: 1,
+                    max: data.amount,
+                }).keypress(function (e) {
+                    return validate_form_text('numbers', e, null);
+                });
+            },
         });
         $('#myModalOutputs').modal('show');
     });
