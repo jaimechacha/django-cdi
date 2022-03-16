1b884e62872a07b7902e792579179c7a80f24a19
Anderson Pozo <anderson.pozo@upec.edu.ec>
2022-03-15 22:21:16 -0500

    Create RefundOutputMaterial

--- core/inventory/models.py
+++ core/inventory/models.py
@@ -183,3 +183,21 @@ class OutputMaterial(AuditMixin, models.Model):
             'num_doc': self.output.num_doc,
         }
         return item
+
+
+class RefundOutputMaterial(AuditMixin, models.Model):
+    date_refund = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    amount = models.IntegerField('Cantidad devuelta', blank=True, null=True)
+    remainder = models.IntegerField('Restante', blank=True, null=True)
+    output_material = models.ForeignKey(OutputMaterial, on_delete=models.PROTECT)
+
+    class Meta:
+        verbose_name = 'Devolución'
+        verbose_name_plural = 'Devoluciones'
+
+    def __str__(self):
+        return '{} {}'.format(self.output_material.material.name, self.amount)
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        return item
